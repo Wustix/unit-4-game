390222da71b480169f4b0f32fceca72aa81d9b90
unknown <wroehrman@yahoo.com>
2018-06-03 00:00:07 -0500

    update to unit4js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var hanSolo = [healthpoints = 130 , attackPower = 8, counterattackpower = 30];
+var lukeSkywalker = [healthpoints = 140, attackPower = 10, counterattackpower = 25];
+var bobaFett = [healthpoints = 100, attackPower = 6, counterattackpower = 20];
+var darthVader = [healthpoints = 150, attackPower = 12, counterattackpower = 30];
+var counterAttack
+var healthElement = getElementById("health");
+var attackElement = getElementById("attack");
+
+function startGame () {
+    
+}
