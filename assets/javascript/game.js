bcf4a5ca2955a262ef613b8d5fab18f449cbf90f
unknown <wroehrman@yahoo.com>
2018-06-03 16:17:00 -0500

    updates to unit4 files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,14 @@ var lukeSkywalker = [healthpoints = 140, attackPower = 10, counterattackpower = 
 var bobaFett = [healthpoints = 100, attackPower = 6, counterattackpower = 20];
 var darthVader = [healthpoints = 150, attackPower = 12, counterattackpower = 30];
 var counterAttack
-var healthElement = getElementById("health");
-var attackElement = getElementById("attack");
 
-function startGame () {
-    
-}
+
+$(document).ready(function() {
+
+
+
+
+
+
+
+});
