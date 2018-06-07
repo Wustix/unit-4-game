f29641675f755f402646a393cfc527071ff741cf
unknown <wroehrman@yahoo.com>
2018-06-06 22:24:08 -0500

    update to unit4 js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,60 @@
-var hanSolo = { name: "Han Solo", health: 130, attackPower: 8, counterAttack: 30};
-var lukeSkywalker = { name: "Luke Skywalker", health: 140, attackPower: 10, counterAttack: 25};
-var bobaFett = { name: "Boba Fett", health: 100, attackPower: 6, counterAttack: 20};
-var darthVader = { name: "Darth Vader", health: 150, attackPower: 12, counterAttack: 30};
-var counterAttack;
-var characterChosen = false;
-var defenderChosen = false;
-var gameOver = false;
-
-
-  
-  
-
 $(document).ready(function() {
 
-    $("#hansolochar").on("click", function() {
+    var hanSolo = [ health = 130, attackPower = 8, counterAttack = 30];
+    var lukeSkywalker = [ health = 140, attackPower = 10, counterAttack = 25];
+    var bobaFett = [ health = 100, attackPower = 6, counterAttack = 20];
+    var darthVader = [ health = 150, attackPower = 12, counterAttack = 30];
+    var characterChosen = false;
+    var defenderChosen = false;
+    var gameOver = false;
+    var defenderBeat = false;
+    
+
+
+    function moveToEnemies() {
+        $(".character-play").removeClass("character-play").addClass("slice-enemies");
+        $("#enemiestoattack").append($(".slice-enemies"));
+      }
+
+      $("#hansolochar").on("click", function () {
         $("#hansolochar").removeClass("character-play").addClass("playerchoice");
         $("#playerchoice").append(this);
-       
-     
+      
+    moveToEnemies();
     });
 
-    $("#lukeskywalkerchar").on("click", function() {
+    $("#lukeskywalkerchar").on("click", function () {
         $("#lukeskywalkerchar").removeClass("character-play").addClass("playerchoice");
         $("#playerchoice").append(this);
+      
+    moveToEnemies();
     });
 
-    $("#darthvaderchar").on("click", function() {
+
+ $("#darthvaderchar").on("click", function () {
         $("#darthvaderchar").removeClass("character-play").addClass("playerchoice");
         $("#playerchoice").append(this);
+      
+    moveToEnemies();
     });
 
-    $("#bobafettchar").on("click", function() {
+    $("#bobafettchar").on("click", function () {
         $("#bobafettchar").removeClass("character-play").addClass("playerchoice");
         $("#playerchoice").append(this);
+      
+    moveToEnemies();
     });
+      
+
+    
+  
+
+        
+   
+
+
+
+
 
 
 
